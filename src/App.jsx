8e63630df4a71d084ed8e62332270042099e5607
RaveHunter05 <paulsotelo97@gmail.com>
2022-02-09 01:15:16 -0600

    add things to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,38 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState([]);
+
+  const dispatch = useDispatch();
+
+  const todos = useSelector((store) => store.todo.todos);
+
+  useEffect(() => {
+    setTodo(todos);
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+      <div>
+        <h2>Ez todo app made with react</h2>
+        <section>
+          <input type="text" placeholder="New Task" />
+          <button
+            onClick={() =>
+              dispatch({ type: "ADD_TODO", payload: { value: "hola" } })
+            }
+          >
+            {" "}
+            Add{" "}
           </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+        </section>
+        <section></section>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import { Provider } from "react-redux";
+
+import generateStore from "./redux/store.js";
+
+const store = generateStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
